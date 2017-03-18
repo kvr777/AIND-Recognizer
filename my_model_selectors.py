aa62b2f1654aeb2f19bb2fd9497bbce9d528fdc8
kvr777 <kirill.v.romanov@gmail.com>
2017-03-18 20:18:52 +0300

    draft of coding

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -33,7 +33,7 @@ class ModelSelector(object):
     def base_model(self, num_states):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
@@ -76,7 +76,31 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        BIC = float('Inf')
+        best_model = None
+        fitted_model = None
+        for Nb_states in range(self.min_n_components, self.max_n_components +1):
+            try:
+                fitted_model = GaussianHMM(n_components=Nb_states, covariance_type="diag", n_iter=1000,
+                                       random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+
+                LogL = fitted_model.score(self.X, self.lengths)
+                N = len(self.X)
+                NDimensions = self.X.shape[1]
+
+                p = (Nb_states-1) + (Nb_states * NDimensions ) + Nb_states * NDimensions * (NDimensions+1)/2. #for full covariance matrices
+
+                BIC_temp = -2 * LogL + p * np.log(N)
+
+            except:
+                BIC_temp = float('Inf')
+
+            if BIC_temp<BIC:
+                BIC=BIC_temp
+                best_model= fitted_model
+
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -91,8 +115,47 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+        DIC = -float('Inf')
+        DIC_Temp = -float('Inf')
+        best_model = None
+        fitted_model = None
+        for Nb_states in range(self.min_n_components, self.max_n_components +1):
+            M = 1.
+            temp_model = None
+            try:
+                fitted_model = GaussianHMM(n_components=Nb_states, covariance_type="diag", n_iter=1000,
+                                       random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+
+                DIC_first_Log = fitted_model.score(self.X, self.lengths)
+                temp_model = fitted_model
+            except:
+                continue
+
+            TotalSumLog = 0
+
+            for hword in self.hwords.keys():
+                if hword != self.this_word:
+                    other_X, other_length = self.hwords[hword]
+                    try:
+                        SumLogOtherWord = temp_model.score(other_X, other_length)
+                        M+=1
+                    except:
+                        SumLogOtherWord =0
+                    TotalSumLog += SumLogOtherWord
+
+            if M==1:
+                M= floaT('inf')
+            DIC_temp = DIC_first_Log - (1/(M-1))*TotalSumLog*1.
+
+            if DIC_temp>DIC:
+                DIC=DIC_temp
+                best_model= temp_model
+
+        return best_model
 
 
 class SelectorCV(ModelSelector):
@@ -104,4 +167,46 @@ class SelectorCV(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        if len(self.lengths) < 2:
+            return print("Number of samples is less than minimal number of kfolds")
+
+        split_method = KFold(n_splits = min(len(self.lengths),3))
+        BestAvgLL = -float('Inf')
+        best_model = None
+        temp_model = None
+        for Nb_states in range(self.min_n_components, self.max_n_components + 1):
+            TotalLL = 0
+            CountLL = 1
+            fitted_model = None
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+
+                X_train, X_test = [],[]
+                for ii in cv_train_idx:
+                    X_train += self.sequences[ii]
+
+                for yy in cv_test_idx:
+                    X_test += self.sequences[yy]
+
+                X_train, X_test = np.array(X_train), np.array(X_test)
+                len_train, len_test = np.array(self.lengths)[cv_train_idx], np.array(self.lengths)[cv_test_idx]
+
+                try:
+                    fitted_model = GaussianHMM(n_components=Nb_states, covariance_type="diag", n_iter=1000,
+                                               random_state=self.random_state, verbose=False).fit(X_train, len_train)
+
+                    LogL = fitted_model.score(X_test, len_test)
+
+                    CountLL += 1
+                except:
+                    LogL = 0
+
+                TotalLL += LogL
+
+            AvgTempLL = TotalLL/(CountLL*1.0)
+
+            if AvgTempLL>BestAvgLL:
+                BestAvgLL = AvgTempLL
+                best_model = fitted_model
+
+        return best_model
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,25 +1,37 @@
 import warnings
-from ASLdb import SinglesData
+from asl_data import SinglesData
 
 
 def recognize(models: dict, test_set: SinglesData):
-     """ Recognize test word sequences from word models set
-
-    :param models: dict of trained models
-        {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
-    :param test_set: SinglesData object
-    :return: (list, list)  as probabilities, guesses
-        both lists are ordered by the test set word_id
-        probabilities is a list of dictionaries where each key a word and value is Log Liklihood
-            [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-             {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-             ]
-        guesses is a list of the best guess words ordered by the test set word_id
-            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
-    """    
-	warnings.filterwarnings("ignore", category=DeprecationWarning)
+    """ Recognize test word sequences from word models set
+   :param models: dict of trained models
+       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
+   :param test_set: SinglesData object
+   :return: (list, list)  as probabilities, guesses
+       both lists are ordered by the test set word_id
+       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            ]
+       guesses is a list of the best guess words ordered by the test set word_id
+           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+   """
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    for word_id,_ in test_set.get_all_Xlengths().items():
+        prob_dict = dict()
+        X_test, len_test = test_set.get_item_Xlengths(word_id)
+        for word_model, model in models.items():
+            try:
+                LogL = model.score(X_test, len_test)
+            except:
+                LogL = -float('inf')
+            prob_dict[word_model] = LogL
+
+        probabilities.append(prob_dict)
+        guesses.append(max(prob_dict, key=prob_dict.get))
+
+    return probabilities, guesses
